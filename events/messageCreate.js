7a8f020f050619171361e7b25674a51a59a5f180
Taevas <67872932+TTTaevas@users.noreply.github.com>
2021-11-15 23:49:04 +0100

    Clean things, add administrative features and puzzle timer

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,12 @@
 prefix = "_answer"
-announcements_channel = "908478150349033503"
-answers_channel = "892404757430435914"
+announcements_channel = "909931933201481771"
+answers_channel = "909932040256905286"
+
+host_id = "202908462722711552"
+programmer_id = "793902871619043400"
+
+started = false
+thinking_time = false
 
 const revealPuzzle = require('../functions/reveal_puzzle.js')
 const logHours = require('../functions/log_hours.js')
@@ -9,7 +15,9 @@ module.exports = {
 	name: "messageCreate",
 	once: false,
 	async execute(discord_client, db, message) {
-		if (message.channel.id == answers_channel && message.content.startsWith(`${prefix} `)) {
+
+		// TREAT ANSWERS
+		if (started && !thinking_time && message.channel.id == answers_channel && message.content.startsWith(`${prefix} `)) {
 			let id = `${message.author.tag}: ${message.content}`
 			console.log(`\nANSWER RECEIVED | ${id} | ${logHours()}`)
 
@@ -19,6 +27,7 @@ module.exports = {
 			let current_puzzle = await puzzles.findOne({id: current_puzzle_id})
 			let submitted_answer = message.content.slice(8).toLowerCase()
 			
+			if (submitted_answer == "penis") {message.react("🍆")}
 			if (submitted_answer == current_puzzle.answer) {
 				console.log(`${message.author.tag} has solved puzzle ${current_puzzle_id + 1}!`)
 				let currentDate = new Date()
@@ -35,8 +44,73 @@ module.exports = {
 			}
 
 			console.log(`ANSWER TREATED | ${id} | ${logHours()}`)
-		} else if (message.author.id == "793902871619043400" && message.content.startsWith("_start")) {
-			revealPuzzle(message.guild.channels.cache.find(channel => channel.id == announcements_channel), db, 0)
+
+		// START WITH THE PUZZLE REVEAL PERIOD
+		} else if (!started && (message.author.id == host_id || message.author.id == programmer_id) && message.content.startsWith("_start")) {
+			const tracking = await db.collection("track").find().toArray()
+			let current_puzzle_id = tracking[0].count
+
+			started = true
+			console.log(`\n${message.author.tag} has started the puzzle reveal period, with puzzle ${current_puzzle_id + 1}\n`)
+			revealPuzzle(message.guild.channels.cache.find(channel => channel.id == announcements_channel), db, current_puzzle_id)
+
+		// RESET TRACKING, REMOVE SOLVERS
+		} else if (!started && (message.author.id == host_id || message.author.id == programmer_id) && message.content.startsWith("_reset")) {
+			console.log(`\n${message.author.tag} is resetting progress...`)
+			const track_collection = await db.collection("track")
+			const tracking = await track_collection.find().toArray()
+			let track_object = tracking[0]._id
+			await track_collection.updateOne({_id: track_object}, {$set: {count: 0}})
+
+			const puzzles = db.collection("testCollection0")
+			await puzzles.updateMany({}, {$set: {solver: ""}})
+			console.log(`Progress has been reset\n`)
+			message.reply("done resetting")
+		
+		// ADD PUZZLE
+		} else if ((message.author.id == host_id || message.author.id == programmer_id) && message.content.startsWith("_add")) {
+			const p_args = message.content.split("/")
+			const puzzles = db.collection("testCollection0")
+			let all_puzzles = await puzzles.find()
+			let all_puzzles_arr = await all_puzzles.toArray()
+			let new_puzzle_id = all_puzzles_arr.length
+
+			let doc = {
+				id: new_puzzle_id,
+				question: p_args[1],
+				answer: p_args[2],
+				hints: [],
+				expire_in_minutes_after_reveal: Number(p_args[3]),
+				image_url: p_args[4] != "none" ? p_args[4] : "",
+				solver: "",
+				solved_date: new Date("1898-12-31T23:50:39.000+00:00"),
+			}
+			let p_image = p_args[4] != "none" ? p_args[4] : ""
+			// _add/who's my waifu?/lain/180/none/do something~90&idk~45
+
+			const hints = p_args[5].split("&")
+			hints.forEach((hint) => {
+				let actual_hint = hint.split("~")
+				let hint_to_push = {
+					text: actual_hint[0],
+					reveal_in_minutes_after_reveal: Number(actual_hint[1])
+				}
+				doc.hints.push(hint_to_push)
+			})
+
+			const add_result = await puzzles.insertOne(doc)
+			let string_message = `The following puzzle has been added:
+question: ${doc.question}
+answer: ${doc.answer}
+expires in minutes: ${doc.expire_in_minutes_after_reveal}
+url of the image: ${doc.image_url}
+				`
+			doc.hints.forEach((hint) => {
+				string_message += `
+hint: ${hint.text} (revealed after ${hint.reveal_in_minutes_after_reveal} minutes)`
+			})
+			message.reply(string_message)
+			console.log(`A puzzle has been added with the _id ${add_result.insertedId}`, doc)
 		}
 	}
 }
--- functions/reveal_puzzle.js
+++ functions/reveal_puzzle.js
@@ -2,16 +2,29 @@ const logHours = require('../functions/log_hours.js')
 
 module.exports = async function revealPuzzle(channel, db, puzzle_id) {
 	const puzzles = db.collection("testCollection0")
+
+	let all_puzzles = await puzzles.find()
+	if (all_puzzles.toArray().length == puzzle_id) {
+		started = false
+		console.log("Looks like all the puzzles have been solved! Shutting down the bot in a few seconds...")
+		setTimeout(function() {process.exit(1)}, 3000)
+		return console.log("...")
+	}
+
 	const current_puzzle = await puzzles.findOne({id: puzzle_id})
 	const hints = current_puzzle.hints
 
+	
 	console.log(`\n(${logHours()}) Revealing puzzle ${puzzle_id + 1}!\n`)
 
 	let exp_date = new Date()
 	exp_date.setMinutes(exp_date.getMinutes() + current_puzzle.expire_in_minutes_after_reveal)
-	let epoch_time = `<t:${String(exp_date.getTime()).substr(0, 10)}:R>`
+	let epoch_time = `<t:${String(exp_date.getTime()).substr(0, 10)}:f> | <t:${String(exp_date.getTime()).substr(0, 10)}:R>`
 	let message_time = timePhrasing(current_puzzle.expire_in_minutes_after_reveal)
-	channel.send(`${current_puzzle.question}\nyou have ${message_time} (${epoch_time})\n\n${current_puzzle.image_url}`)
+
+	let question_string = `${current_puzzle.question}\nyou have ${message_time} (${epoch_time})`
+	if (current_puzzle.image_url) {question_string += `\n\n${current_puzzle.image_url}`}
+	channel.send(question_string)
 
 	let ms = current_puzzle.expire_in_minutes_after_reveal * 60000
 	let reminder_times = [ms-(ms/2), ms-(ms/5)]
@@ -32,7 +45,7 @@ module.exports = async function revealPuzzle(channel, db, puzzle_id) {
 
 	hints.forEach((hint) => {
 		let hint_time_ms = hint.reveal_in_minutes_after_reveal * 60000
-		let phrased_hint_time = timePhrasing(hint.reveal_in_minutes_after_reveal)
+		let phrased_hint_time = timePhrasing(hint.reveal_in_minutes_after_reveal * 60000)
 		console.log(`(${logHours()}) A hint has been set to be sent in ${phrased_hint_time}`)
 		setTimeout(async function() {
 			let updated_puzzle = await puzzles.findOne({id: puzzle_id})
@@ -46,8 +59,19 @@ module.exports = async function revealPuzzle(channel, db, puzzle_id) {
 	})
 
 	setTimeout(async function() {
+		let updated_puzzle = await puzzles.findOne({id: puzzle_id})
 		if (!updated_puzzle.solver) {
-			console.log(`\n(${logHours()}) Puzzle ${puzzle_id + 1} has expired\n`)
+			thinking_time = true
+			channel.send(`puzzle has not been completed within the timer, you'll be able to give answers once again in 1 hour`)
+			console.log(`\n(${logHours()}) Puzzle ${puzzle_id + 1}'s timer has expired, entering "thinking time" for 1 hour\n`)
+
+			setTimeout(async function() {
+				thinking_time = false
+				channel.send(`you can give answers again, you need to solve the puzzle to proceed`)
+				console.log(`\n(${logHours()}) Puzzle ${puzzle_id + 1}'s thinking time has expired, exiting "thinking time"\n`)
+			}, 3600000)
+		} else {
+			console.log(`\n(${logHours()}) Puzzle ${puzzle_id + 1} will not enter thinking time as it has already been solved\n`)
 		}
 	}, ms)
 
@@ -55,6 +79,7 @@ module.exports = async function revealPuzzle(channel, db, puzzle_id) {
 }
 
 function timePhrasing(minutes) {
+	minutes = Number(minutes)
 	minutes = Number(minutes.toFixed(1))
 	if (minutes < 60) {return `${minutes} minute${minutes >= 2 ? "s" : ""}`}
 	if (minutes % 60) {return `${minutes / 60} hours`}
